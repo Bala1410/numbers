72da72828691e9b849a903be238782b3a61e22f3
Akilan99 <37103105+Akilan99@users.noreply.github.com>
2019-06-06 11:27:33 +0530

    Update natural.py

--- natural.py
+++ natural.py
@@ -1,9 +1,6 @@
-num = int(input("enter the number")
-if num < 0:
- print("Enter a positive number")
-else:
-   sum = 0
-   while(num > 0):
-       sum += num
-       num -= 1
-   print("The sum is",sum)
+n = int(input())
+sum = 0
+for num in range(0,n+1,1):
+  sum = sum+num
+  print("num")
+
