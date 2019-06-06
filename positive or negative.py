86f3ea7bc22ec1cad0172a8038435aaea3aa7e37
Akilan99 <37103105+Akilan99@users.noreply.github.com>
2019-06-06 09:59:19 +0530

    Update positive or negative.py

--- positive or negative.py
+++ positive or negative.py
@@ -1,7 +1,7 @@
 num=float(input("enter the number"))
-if num > 0:
-   print("positive")
- elif num ==0:
+if num == 0:
    print("zero")
+ elif num > 0:
+   print("positive")
  else:
    print("negative")
