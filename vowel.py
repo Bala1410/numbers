3fc71a42b82949ecfd0bf8419a908e58fd2d55b5
Akilan99 <37103105+Akilan99@users.noreply.github.com>
2019-06-06 10:12:09 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+char=input("enter the letteer")
+if char=='A' or char=='a' or char=='E' or char=='e' or char=='I' or char=='i' or char=='O' or char=='o' char=='U' or char=='u':
+  print("it is an vowel")
+else:
+  print("constant")
